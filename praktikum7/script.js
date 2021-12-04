ee760f6a0a7ff6ad70fe64279964f225387558c3
Muhamad Iqbal <93023456+Iqbal2121@users.noreply.github.com>
2021-12-04 10:41:43 +0700

    Add files via upload

---
+++ praktikum7/script.js
@@ -0,0 +1,15 @@
+let hallo = "Hello Dunia!!!......";
+console.log(hallo)
+
+// belajar variabel
+// statis
+
+const phi = 3.14;
+// phi = 3.15; error
+console.log(phi);
+
+// dinamis
+
+let nama = "Muhamad Iqbal";
+nama = "disiqbal"
+console.log(nama);
